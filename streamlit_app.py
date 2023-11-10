b4feddb403cd4463b7493172783cb3d5e34d8c61
darkangelBDF120 <122787973+darkangelBDF120@users.noreply.github.com>
2023-11-10 10:14:04 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,4 +45,4 @@ streamlit.dataframe(my_data_rows)
 add_my_fruit = streamlit.text_input("What fruit would you like to add?")
 fruit_added = streamlit.write("Thanks for adding", add_my_fruit)
 
-my_cur.execute("insert into PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST values ('test')")
+my_cur.execute("insert into PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST values ('from Streamlit')")
