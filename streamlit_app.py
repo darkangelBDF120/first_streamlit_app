d2c70c3a81bb5873b74076e8e51d1a1ebe011d3c
darkangelBDF120 <122787973+darkangelBDF120@users.noreply.github.com>
2023-11-10 12:13:05 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,6 @@ import requests
 from urllib.error import URLError
 
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.text(🥣'Omega 3 & Blueberry Oatmeal')
+streamlit.text(🥗'Kale, Spinach & Rocket Smoothie')
+streamlit.text(🥑🐔'Hard-Boiled Free-Range Egg')
