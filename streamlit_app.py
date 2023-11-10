a840c7389711ad1d8ea10c1a8c10c1e7d8fec610
darkangelBDF120 <122787973+darkangelBDF120@users.noreply.github.com>
2023-11-10 13:42:12 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -52,6 +52,8 @@ add_my_fruit = streamlit.write("Thank you for adding ", add_my_fruit)
 streamlit.text_input("What fruit would you like to add?")
 streamlit.write('Thanks for adding ', add_my_fruit)
 
+streamlit.stop()
+
 #import requests
 my_cur.execute("insert into pc_rivery_db.public.fruit_load_list values ('from streamlit')")
 
