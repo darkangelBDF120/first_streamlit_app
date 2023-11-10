eeba4e8604893dab9f64e44213d16d20b9c529f1
darkangelBDF120 <122787973+darkangelBDF120@users.noreply.github.com>
2023-11-10 13:42:55 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,8 +49,6 @@ streamlit.dataframe(my_data_rows)
 
 add_my_fruit = streamlit.text_input("What fruit would you like to add?")
 add_my_fruit = streamlit.write("Thank you for adding ", add_my_fruit)
-streamlit.text_input("What fruit would you like to add?")
-streamlit.write('Thanks for adding ', add_my_fruit)
 
 streamlit.stop()
 
