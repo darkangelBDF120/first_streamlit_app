fa6f7570dcdf1ac12769e5c5abd7332df67f3afd
darkangelBDF120 <122787973+darkangelBDF120@users.noreply.github.com>
2023-11-10 13:30:42 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -52,3 +52,11 @@ add_my_fruit = streamlit.write("Thank you for adding ", add_my_fruit)
 
 #import requests
 my_cur.execute("insert into pc_rivery_db.public.fruit_load_list values ('from streamlit')")
+
+#import snowflake connector
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
